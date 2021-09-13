1ab98a3872f8d4bdef4ea04f5e1aeb1a218aead9
Christian <chibuike4jamb@gmail.com>
2021-09-13 02:14:24 +0100

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,6 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
