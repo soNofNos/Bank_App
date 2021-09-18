d26046f1b71ef9daf17d15fbd2328ed5488380a2
Christian <chibuike4jamb@gmail.com>
2021-09-18 02:29:06 +0100

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -77,6 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+
       body: Center(
 
         // Center is a layout widget. It takes a single child and positions it
